e8208248a6285e893026bee2b814a8bc40de6c0b
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2025-10-06 16:29:50 -0300

    Adiciona novo projeto da Rocketseat

--- gallery-plus/src/contexts/albums/models/album.ts
+++
@@ -1,4 +0,0 @@
-export interface Album {
-  id: string;
-  title: string;
-}
--- gallery-plus/src/contexts/photos/models/photo.ts
+++
@@ -1,8 +0,0 @@
-import type { Album } from "../../albums/models/album";
-
-export interface Photo {
-  id: string;
-  title: string;
-  imageId: string;
-  albums: Album[];
-}
